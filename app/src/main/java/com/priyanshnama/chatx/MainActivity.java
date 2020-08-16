ece4a6a55f1a0a26eb178dbb36caed0bda914d69
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-08-17 01:25:59 +0530

    basic Layout of MainActivity done.

--- app/src/main/java/com/priyanshnama/chatx/MainActivity.java
+++ app/src/main/java/com/priyanshnama/chatx/MainActivity.java
@@ -2,12 +2,30 @@ package com.priyanshnama.chatx;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout phone, code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        phone = findViewById(R.id.phone);
+        code = findViewById(R.id.code);
+    }
+
+
+    public void sendCode(View view) {
+        phone.setVisibility(View.INVISIBLE);
+        code.setVisibility(View.VISIBLE);
+    }
+
+    public void verifyCode(View view) {
+        phone.setVisibility(View.VISIBLE);
+        code.setVisibility(View.INVISIBLE);
     }
 }
